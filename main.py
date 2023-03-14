2d355dfb7bd98d24d1e6571f7de55be19607857f
ruhuang <ruhuang2001@gmail.com>
2023-03-14 11:10:25 +0800

    optimize code

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from reportlab.lib.pagesizes import A4, portrait
 from reportlab.pdfgen import canvas
 from pypdf import PdfWriter
 
-allImg = []  # 存储的图像名路径
+cartoon_path = './test/'
+# temp_pdf = './temp.pdf'
+cartoon_pdf = './cartoon.pdf'
+all_img = []  # 存储的图像名路径
 merger = PdfWriter()
 
 
-# 改好文件夹的名字，以便排序
+# 修改文件夹的名字，以便排序
 def renamePre(path):
     filename = os.listdir(path)
     for file in filename:
@@ -21,22 +24,22 @@ def renamePre(path):
             shutil.rmtree(path + file + '/')
 
 
-# 找到一个文件夹下所有的图片
+# 找一个文件夹下所有的图片, 加入all_img
 def findAllFile(path):
     filename = os.listdir(path)
     for file in filename:
         if file.endswith('.jpg'):
             fullname = os.path.join(path, file)
-            allImg.append(fullname)
+            all_img.append(fullname)
 
 
-# 图片变pdf
-def imageToPdf(img_path, pdf_path):
+# 合并所有图片到pdf
+def imageToPdf(pdf_path):
     pages = 0
     (w, h) = portrait(A4)
     c = canvas.Canvas(pdf_path, pagesize=portrait(A4))
-    findAllFile(img_path)
-    for i in allImg:
+    # findAllFile(img_path)
+    for i in all_img:
         c.drawImage(i, 0, 0, w, h)
         c.showPage()
         pages = pages + 1
@@ -46,22 +49,25 @@ def imageToPdf(img_path, pdf_path):
 def mergeAllPdf(path, pdf_path):
     folders = sorted(os.listdir(path))
     folders = natsorted(folders)
-
+    # 自然排序后遍历加入all_img
     for folder in folders:
-        newpath = path + folder + '/'
-        imageToPdf(newpath, pdf_path)
-        # merger = PdfWriter()
-        merger.append(pdf_path)
-        allImg.clear()
+        current_path = path + folder + '/'
+        # imageToPdf(pdf_path)
+        findAllFile(current_path)
+        # merger.append(pdf_path)
+        # all_img.clear()
+
+    imageToPdf(pdf_path)
+    # merger.append(pdf_path)
+    # # 写入cartoon.pdf中
+    # merger.write(merge_pdf)
+    # merger.close()
 
 
 def main():
-    path = './test/'
-    temp_pdf = './temp.pdf'
-    merge_pdf = './cartoon.pdf'
-    # renamePre(path)
-    mergeAllPdf(path, temp_pdf)
-    merger.write(merge_pdf)
+    # 因为test文件夹已经改好名字了，所有无需执行这个，第一次执行请取消下面的注释
+    # renamePre(cartoon_path)
+    mergeAllPdf(cartoon_path, cartoon_pdf)
 
 
 if __name__ == '__main__':
